762f5d634586d9bb2679414a9e3dc246bba5e741
Joel Jayson Clark <jc214809@ohio.edu>
2016-03-30 15:11:48 -0400

    removed bootstrap and started exercise form

--- exercise/exercise.js
+++ exercise/exercise.js
@@ -2,6 +2,44 @@ angular.module('sample.exercise', [
         'auth0'
     ])
     .controller('ExerciseCtrl', function ExerciseController($scope, auth, $http, $location, store) {
+        $("#exerciseForm").submit(function(event) {
+            //alert($("#exerciseForm").serializeObject());
 
+            $scope.exerciseData = $("#exerciseForm").serializeAssoc();
+        });
 
     });
+
+$.fn.serializeAssoc = function() {
+    var data = {};
+    $.each(this.serializeArray(), function(key, obj) {
+        var a = obj.name.match(/(.*?)\[(.*?)\]/);
+        if (a !== null) {
+            var subName = a[1];
+            var subKey = a[2];
+            if (!data[subName]) data[subName] = [];
+            if (data[subName][subKey]) {
+                if ($.isArray(data[subName][subKey])) {
+                    data[subName][subKey].push(obj.value);
+                } else {
+                    data[subName][subKey] = [];
+                    data[subName][subKey].push(obj.value);
+                }
+            } else {
+                data[subName][subKey] = obj.value;
+            }
+        } else {
+            if (data[obj.name]) {
+                if ($.isArray(data[obj.name])) {
+                    data[obj.name].push(obj.value);
+                } else {
+                    data[obj.name] = [];
+                    data[obj.name].push(obj.value);
+                }
+            } else {
+                data[obj.name] = obj.value;
+            }
+        }
+    });
+    return data;
+};
