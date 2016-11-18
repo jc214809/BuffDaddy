72adb6317dad583b75d620da4436f8020c284892
Joel Jayson Clark <jc214809@ohio.edu>
2016-11-17 20:52:35 -0500

    try

--- login/login.js
+++ login/login.js
@@ -6,32 +6,33 @@ angular.module('sample.login', [
       $location.path("/");
     }
     $scope.login = function() {
-      auth.signin({}, function(profile, token) {
-          console.log("Profile: " + JSON.stringify(profile))
-          store.set('profile', profile);
-          store.set('token', token);
+      // auth.signin({}, function(profile, token) {
+      //     console.log("Profile: " + JSON.stringify(profile))
+      //     store.set('profile', profile);
+      //     store.set('token', token);
 
-          $scope.userDetails = {
-            emailAddress: profile.email,
-            firstName: profile.given_name,
-            lastName: profile.family_name,
-            gender: profile.gender,
-            socialId: profile.identities[0].user_id
-          };
-          $http.post($scope.url + "/register", JSON.stringify($scope.userDetails))
-            .then(
-              function successCallback(response) {
-                console.log("Success");
-              },
-              function errorCallback(response) {
-                alert("Error " + JSON.stringify(response));
-              });
-          $location.path("/");
+      //     $scope.userDetails = {
+      //       emailAddress: profile.email,
+      //       firstName: profile.given_name,
+      //       lastName: profile.family_name,
+      //       gender: profile.gender,
+      //       socialId: profile.identities[0].user_id
+      //     };
+      //     $http.post($scope.url + "/register", JSON.stringify($scope.userDetails))
+      //       .then(
+      //         function successCallback(response) {
+      //           console.log("Success");
+      //         },
+      //         function errorCallback(response) {
+      //           alert("Error " + JSON.stringify(response));
+      //         });
+      //     $location.path("/");
 
-        },
-        function(error) {
-          console.log("There was an error logging in", error);
-        });
+      //   },
+      //   function(error) {
+      //     console.log("There was an error logging in", error);
+      //   });
+      $location.path("/");
     }
 
   });
