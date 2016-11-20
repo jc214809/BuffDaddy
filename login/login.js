851c73392d9dca17ed2fce42f26c6d49d4686e58
Joel Jayson Clark <jc214809@ohio.edu>
2016-11-20 12:35:05 -0500

    add try catch

--- login/login.js
+++ login/login.js
@@ -8,34 +8,39 @@ angular.module('sample.login', [
     alert("0");
     $scope.login = function() {
       alert("1");
-      auth.signin({}, function(profile, token) {
-              alert("2");
-          console.log("Profile: " + JSON.stringify(profile))
-          //store.set('profile', profile);
-          //store.set('token', token);
+      try {
+        auth.signin({}, function(profile, token) {
+            alert("2");
+            console.log("Profile: " + JSON.stringify(profile))
+            store.set('profile', profile);
+            store.set('token', token);
 
-          $scope.userDetails = {
-            emailAddress: profile.email,
-            firstName: profile.given_name,
-            lastName: profile.family_name,
-            gender: profile.gender,
-            socialId: profile.identities[0].user_id
-          };
-          $http.post($scope.url + "/register", JSON.stringify($scope.userDetails))
-            .then(
-              function successCallback(response) {
-                              alert("3");
-                console.log("Success");
-              },
-              function errorCallback(response) {
-                alert("Error " + JSON.stringify(response));
-              });
-          $location.path("/");
+            $scope.userDetails = {
+              emailAddress: profile.email,
+              firstName: profile.given_name,
+              lastName: profile.family_name,
+              gender: profile.gender,
+              socialId: profile.identities[0].user_id
+            };
+            $http.post($scope.url + "/register", JSON.stringify($scope.userDetails))
+              .then(
+                function successCallback(response) {
+                  alert("3");
+                  console.log("Success");
+                },
+                function errorCallback(response) {
+                  alert("Error " + JSON.stringify(response));
+                });
+            $location.path("/");
 
-        },
-        function(error) {
-          console.log("There was an error logging in", error);
-        });
+          },
+          function(error) {
+            console.log("There was an error logging in", error);
+          });
+      } catch (err) {
+        alert(err.message);
+      }
+
     }
 
   });
