872fec2510b9d27f9d2d1c9cd9ff1a900b6d55d6
Joel Clark <jc214809@ohio.edu>
2017-02-05 13:45:54 -0500

    Update previousworkouts.controller.js

--- previousworkouts/previousworkouts.controller.js
+++ previousworkouts/previousworkouts.controller.js
@@ -10,7 +10,9 @@
   function PreviousWorkoutsController($scope, authService, $http) {
 
     $http.get($scope.url + "/getPreviousWorkouts?socialId=" + $scope.socialId).then(function successCallback(response) {
-          $scope.previousData = response.data;
+          //$scope.previousData = response.data;
+          $scope.previousData = $filter('orderBy')(response.data, '-workoutId');
+
         },
         function errorCallback(response) {
           console.log("Error getting users exercises " + JSON.stringify(response));
