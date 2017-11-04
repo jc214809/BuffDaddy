38150e9a2d5ecb5236a6dbb83be562f108c54648
Joel Jayson Clark <jc214809@ohio.edu>
2017-11-04 17:49:18 -0400

    fixed wrong url

--- auth/auth.service.js
+++ auth/auth.service.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
 
   'use strict';
 
@@ -6,7 +6,7 @@
     .module('app')
     .service('authService', authService);
 
-  authService.$inject = ['lock', 'authManager', '$q','$state','$http'];
+  authService.$inject = ['lock', 'authManager', '$q', '$state', '$http'];
 
   function authService(lock, authManager, $q, $state, $http) {
 
@@ -36,29 +36,29 @@
     // Set up the logic for when a user authenticates
     // This method is called from app.run.js
     function registerAuthenticationListener() {
-      lock.on('authenticated', function (authResult) {
+      lock.on('authenticated', function(authResult) {
         localStorage.setItem('id_token', authResult.idToken);
         authManager.authenticate();
 
-        lock.getProfile(authResult.idToken, function (error, profile) {
+        lock.getProfile(authResult.idToken, function(error, profile) {
           if (error) {
             return console.log(error);
           }
           var userDetails = {
-              emailAddress: profile.email,
-              firstName: profile.given_name,
-              lastName: profile.family_name,
-              gender: profile.gender,
-              socialId: profile.identities[0].user_id
-            };
-            $http.post("http://75.118.135.179:7080/BuffDaddyAPI/register", JSON.stringify(userDetails))
-              .then(
-                function successCallback(response) {
-                  console.log("Success register");
-                },
-                function errorCallback(response) {
-                  alert("Error " + JSON.stringify(response));
-                });
+            emailAddress: profile.email,
+            firstName: profile.given_name,
+            lastName: profile.family_name,
+            gender: profile.gender,
+            socialId: profile.identities[0].user_id
+          };
+          $http.post("http://75.118.135.179:7079/BuffDaddyAPI/register", JSON.stringify(userDetails))
+            .then(
+              function successCallback(response) {
+                console.log("Success register");
+              },
+              function errorCallback(response) {
+                alert("Error " + JSON.stringify(response));
+              });
           localStorage.setItem('profile', JSON.stringify(profile));
           deferredProfile.resolve(profile);
         });
