6d096831bc947da56bd395abf6883ef7f6c225e6
Joel Jayson Clark <jc214809@ohio.edu>
2018-01-14 18:26:54 -0500

    added routines

--- previousworkouts/previousworkouts.controller.js
+++ previousworkouts/previousworkouts.controller.js
@@ -5,9 +5,9 @@
     .module('app')
     .controller('PreviousWorkoutsController', PreviousWorkoutsController);
 
-  PreviousWorkoutsController.$inject = ['$scope', 'authService', '$http'];
+  PreviousWorkoutsController.$inject = ['$scope', 'authService', '$http', '$state'];
 
-  function PreviousWorkoutsController($scope, authService, $http) {
+  function PreviousWorkoutsController($scope, authService, $http, $state) {
 
     $http.get($scope.url + "/getPreviousWorkouts?socialId=" + $scope.socialId).then(function successCallback(response) {
         $scope.previousData = response.data;
@@ -16,6 +16,21 @@
       function errorCallback(response) {
         console.log("Error getting users exercises " + JSON.stringify(response));
       });
+
+    $scope.repeatWorkout = function(workoutData) {
+      console.dir(workoutData);
+      $http.post($scope.url + "/repeatPreviousWorkouts", {
+        "workoutId": workoutData[0].workoutId,
+        "userId": $scope.socialId
+      }).then(function successCallback(response) {
+          console.log("Routine Complete");
+          $state.go('home');
+        },
+        function errorCallback(response) {
+          console.log("Error saving set details " + JSON.stringify(response));
+        });
+    };
+
   }
 
 }());
