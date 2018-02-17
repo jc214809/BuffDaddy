5260a4a3a2682d38657bdd0d23d93af68292be58
Joel Jayson Clark <jc214809@ohio.edu>
2018-02-16 22:41:09 -0500

    Added Workout Titles tests

--- test/spec/home.controller.spec.js
+++ test/spec/home.controller.spec.js
@@ -1,4 +1,4 @@
-describe('Controller Unit test', function() {
+describe('Home Controller', function() {
 
   var controller;
 
@@ -12,7 +12,7 @@ describe('Controller Unit test', function() {
     });
   }));
 
-  describe('Get the controller', function() {
+  describe('Test the Home controller', function() {
     it('Width of inputs is correct', function() {
       scope.setWidth(1);
       expect(scope.size).toEqual("88.2%");
@@ -26,7 +26,50 @@ describe('Controller Unit test', function() {
       expect(scope.size).toEqual("13%");
       scope.setWidth(99);
       expect(scope.size).toEqual("13%");
-
     });
+    it('Workout Title is Core', function() {
+      scope.workoutExercises = [
+        { "exerciseId": 24, "exerciseName": "Ab X Machine", "recommendation": null, "exerciseDescription": null, "sortId": 893, "image": null, "weight": false, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:139" },
+        { "exerciseId": 8, "exerciseName": "Crunch", "recommendation": null, "exerciseDescription": null, "sortId": 894, "image": null, "weight": false, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:140" },
+        { "exerciseId": 5, "exerciseName": "Cable Crunch", "recommendation": null, "exerciseDescription": null, "sortId": 895, "image": null, "weight": true, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:141" },
+        { "exerciseId": 62, "exerciseName": "Crunch - Hands Overhead", "recommendation": null, "exerciseDescription": null, "sortId": 896, "image": null, "weight": false, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:142" }
+      ]
+      scope.getWorkoutTitle();
+      expect(scope.workoutTitle).toEqual("Core");
+    });
+    it('Workout Title is Core & Cardio', function() {
+      scope.workoutExercises = [
+        { "exerciseId": 24, "exerciseName": "Ab X Machine", "recommendation": null, "exerciseDescription": null, "sortId": 893, "image": null, "weight": false, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:139" },
+        { "exerciseId": 8, "exerciseName": "Crunch", "recommendation": null, "exerciseDescription": null, "sortId": 894, "image": null, "weight": false, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:140" },
+        { "exerciseId": 5, "exerciseName": "Cable Crunch", "recommendation": null, "exerciseDescription": null, "sortId": 895, "image": null, "weight": true, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:141" },
+        { "exerciseId": 62, "exerciseName": "Crunch - Hands Overhead", "recommendation": null, "exerciseDescription": null, "sortId": 896, "image": null, "weight": false, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": false, "legs": false, "shoulders": false, "cardio": true, "$$hashKey": "object:142" }
+      ]
+      scope.getWorkoutTitle();
+      expect(scope.workoutTitle).toEqual("Core & Cardio");
+    });
+    it('Workout Title is Cardio less than 25%', function() {
+      scope.workoutExercises = [
+        { "exerciseId": 24, "exerciseName": "Ab X Machine", "recommendation": null, "exerciseDescription": null, "sortId": 893, "image": null, "weight": false, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:139" },
+        { "exerciseId": 8, "exerciseName": "Crunch", "recommendation": null, "exerciseDescription": null, "sortId": 894, "image": null, "weight": false, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:140" },
+        { "exerciseId": 5, "exerciseName": "Cable Crunch", "recommendation": null, "exerciseDescription": null, "sortId": 895, "image": null, "weight": true, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:141" },
+        { "exerciseId": 62, "exerciseName": "Crunch - Hands Overhead", "recommendation": null, "exerciseDescription": null, "sortId": 896, "image": null, "weight": false, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:142" },
+        { "exerciseId": 62, "exerciseName": "Crunch - Hands Overhead", "recommendation": null, "exerciseDescription": null, "sortId": 896, "image": null, "weight": false, "reps": true, "time": false, "distance": false, "calories": false, "heartRate": false, "stairs": false, "steps": false, "level": false, "incline": false, "strokes": false, "speed": false, "socialId": null, "arms": false, "back": false, "chest": false, "core": false, "legs": false, "shoulders": false, "cardio": true, "$$hashKey": "object:142" }
+      ]
+      scope.getWorkoutTitle();
+      expect(scope.workoutTitle).toEqual("Core");
+    });
+    it('Workout Title Total Body', function() {
+      scope.workoutExercises = [
+        { "exerciseId": 24, "exerciseName": "1", "arms": true, "back": false, "chest": false, "core": false, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:139" },
+        { "exerciseId": 34, "exerciseName": "1", "arms": false, "back": true, "chest": false, "core": false, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:139" },
+        { "exerciseId": 54, "exerciseName": "1", "arms": false, "back": false, "chest": true, "core": false, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:139" },
+        { "exerciseId": 554, "exerciseName": "1", "arms": false, "back": false, "chest": false, "core": true, "legs": false, "shoulders": false, "cardio": false, "$$hashKey": "object:139" },
+        { "exerciseId": 2554, "exerciseName": "1", "arms": false, "back": false, "chest": false, "core": false, "legs": true, "shoulders": false, "cardio": false, "$$hashKey": "object:139" },
+        { "exerciseId": 255554, "exerciseName": "1", "arms": false, "back": false, "chest": false, "core": false, "legs": true, "shoulders": true, "cardio": false, "$$hashKey": "object:139" }
+      ]
+      scope.getWorkoutTitle();
+      expect(scope.workoutTitle).toEqual("Legs, Shoulders, Core, Chest, Back & Arms");
+    });
+
   });
 });
